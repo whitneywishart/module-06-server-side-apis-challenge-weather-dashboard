1156ae3efc0c26c6f0e4f2aad8a5343ec56593ff
whitneywishart <whitneywishart@gmail.com>
2023-02-07 16:52:49 -0800

    add fully functional 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -65,7 +65,7 @@ function currentSearchDisplay(event) {
             currentCityName.innerHTML = cityName + " " + "(" + today.format("M/D/YYYY") + ")";
             icon.innerHTML = weatherIcon;
             icon.setAttribute('src', weatherIcon);
-            temp.innerHTML = "Temp: " + Number(1.8 * (tempIndex - 273) + 32).toFixed(1) + "°F";
+            temp.innerHTML = "Temp: " + Number(1.8 * (tempIndex - 273) + 32).toFixed(1) + " °F";
             wind.innerHTML = "Wind: " + windIndex + " MPH";
             humidity.innerHTML = "Humidity: " + humidityIndex + "%";
 
@@ -76,7 +76,7 @@ function currentSearchDisplay(event) {
             fetch(forecastURL)
                 .then(response => response.json())
                 .then(data => {
-                    // console.log(data);
+                    console.log(data);
 
                     // 5 day forecast DOM variables
                     var forecastDayOneCityDate = document.getElementById("forecast-date-one");
@@ -84,10 +84,34 @@ function currentSearchDisplay(event) {
                     var forecastDayThreeCityDate = document.getElementById("forecast-date-three");
                     var forecastDayFourCityDate = document.getElementById("forecast-date-four");
                     var forecastDayFiveCityDate = document.getElementById("forecast-date-five");
+
+                    // Icon variables
                     var forecastDayOneIcon = document.getElementById("day-one-icon");
+                    var forecastDayTwoIcon = document.getElementById("day-two-icon");
+                    var forecastDayThreeIcon = document.getElementById("day-three-icon");
+                    var forecastDayFourIcon = document.getElementById("day-four-icon");
+                    var forecastDayFiveIcon = document.getElementById("day-five-icon");
+
+                    // Temp variables
                     var forecastDayOneTemp = document.getElementById("day-one-temp");
+                    var forecastDayTwoTemp = document.getElementById("day-two-temp");
+                    var forecastDayThreeTemp = document.getElementById("day-three-temp");
+                    var forecastDayFourTemp = document.getElementById("day-four-temp");
+                    var forecastDayFiveTemp = document.getElementById("day-five-temp");
+
+                    // Wind variables
                     var forecastDayOneWind = document.getElementById("day-one-wind");
+                    var forecastDayTwoWind = document.getElementById("day-two-wind");
+                    var forecastDayThreeWind = document.getElementById("day-three-wind");
+                    var forecastDayFourWind = document.getElementById("day-four-wind");
+                    var forecastDayFiveWind = document.getElementById("day-five-wind");
+
+                    // Humidity variables
                     var forecastDayOneHumidity = document.getElementById("day-one-humidity");
+                    var forecastDayTwoHumidity = document.getElementById("day-two-humidity");
+                    var forecastDayThreeHumidity = document.getElementById("day-three-humidity");
+                    var forecastDayFourHumidity = document.getElementById("day-four-humidity");
+                    var forecastDayFiveHumidity = document.getElementById("day-five-humidity");
 
                     // 5 day forecast fetch dates variables
                     var forecastDateOneTimeStamp = data['list'][4]['dt']
@@ -97,40 +121,93 @@ function currentSearchDisplay(event) {
                     var forecastDateFiveTimeStamp = data['list'][38]['dt']
 
 
-                    // Convert 5 day forecast dates
+                    // Forecast day one
                     var forecastOneConvert = new Date(forecastDateOneTimeStamp * 1000);
                     var forecastDayOneDate = (forecastOneConvert.toLocaleDateString());
+                    var forecastDayOneDefaultTemp = data.list[4].main.temp;
+                    var forecastDayOneDefaultWind = data.list[4].wind.speed;
+                    var forecastDayOneDefaultHumidity = data.list[4].main.humidity;
 
+                    // Forecast day two
                     var forecastTwoConvert = new Date(forecastDateTwoTimeStamp * 1000);
                     var forecastDayTwoDate = (forecastTwoConvert.toLocaleDateString());
+                    var forecastDayTwoDefaultTemp = data.list[15].main.temp;
+                    var forecastDayTwoDefaultWind = data.list[15].wind.speed;
+                    var forecastDayTwoDefaultHumidity = data.list[15].main.humidity;
 
+                    // Forecast day three
                     var forecastThreeConvert = new Date(forecastDateThreeTimeStamp * 1000);
                     var forecastDayThreeDate = (forecastThreeConvert.toLocaleDateString());
+                    var forecastDayThreeDefaultTemp = data.list[20].main.temp;
+                    var forecastDayThreeDefaultWind = data.list[20].wind.speed;
+                    var forecastDayThreeDefaultHumidity = data.list[20].main.humidity;
 
+                    // Forecast day four
                     var forecastFourConvert = new Date(forecastDateFourTimeStamp * 1000);
                     var forecastDayFourDate = (forecastFourConvert.toLocaleDateString());
+                    var forecastDayFourDefaultTemp = data.list[28].main.temp;
+                    var forecastDayFourDefaultWind = data.list[28].wind.speed;
+                    var forecastDayFourDefaultHumidity = data.list[28].main.humidity;
 
+                    // Forecast day five
                     var forecastFiveConvert = new Date(forecastDateFiveTimeStamp * 1000);
                     var forecastDayFiveDate = (forecastFiveConvert.toLocaleDateString());
+                    var forecastDayFiveDefaultTemp = data.list[38].main.temp;
+                    var forecastDayFiveDefaultWind = data.list[38].wind.speed;
+                    var forecastDayFiveDefaultHumidity = data.list[38].main.humidity;
+
 
                     // Get day one icon
                     var forecastDateOneIcon = 'https://openweathermap.org/img/w/' + data.list[4].weather[0].icon + '.png';
 
+
                     //Display day one forecast info
                     forecastDayOneCityDate.innerHTML = forecastDayOneDate;
                     forecastDayOneIcon.setAttribute('src', forecastDateOneIcon);
-                    forecastDayOneTemp.innerHTML = "Temp: " + Number(1.8 * (tempIndex - 273) + 32).toFixed(1) + "°F";
-                    forecastDayOneWind.innerHTML = "Wind: " + windIndex + " MPH";
-                    forecastDayOneHumidity.innerHTML = "Humidity: " + humidityIndex + "%";
+                    forecastDayOneTemp.innerHTML = "Temp: " + Number(1.8 * (forecastDayOneDefaultTemp - 273) + 32).toFixed(1) + " °F";
+                    forecastDayOneWind.innerHTML = "Wind: " + forecastDayOneDefaultWind + " MPH";
+                    forecastDayOneHumidity.innerHTML = "Humidity: " + forecastDayOneDefaultHumidity + "%";
 
 
-
-
+                    // Get day two icon
+                    var forecastDateTwoIcon = 'https://openweathermap.org/img/w/' + data.list[15].weather[0].icon + '.png';
                     //Display day two forecast info
                     forecastDayTwoCityDate.innerHTML = forecastDayTwoDate;
+                    forecastDayTwoIcon.setAttribute('src', forecastDateTwoIcon);
+                    forecastDayTwoTemp.innerHTML = "Temp: " + Number(1.8 * (forecastDayTwoDefaultTemp - 273) + 32).toFixed(1) + " °F";
+                    forecastDayTwoWind.innerHTML = "Wind: " + forecastDayTwoDefaultWind + " MPH";
+                    forecastDayTwoHumidity.innerHTML = "Humidity: " + forecastDayTwoDefaultHumidity + "%";
+
+
+                    // Get day three icon
+                    var forecastDateThreeIcon = 'https://openweathermap.org/img/w/' + data.list[20].weather[0].icon + '.png';
+                    //Display day three forecast info
                     forecastDayThreeCityDate.innerHTML = forecastDayThreeDate;
+                    forecastDayThreeIcon.setAttribute('src', forecastDateThreeIcon);
+                    forecastDayThreeTemp.innerHTML = "Temp: " + Number(1.8 * (forecastDayThreeDefaultTemp - 273) + 32).toFixed(1) + " °F";
+                    forecastDayThreeWind.innerHTML = "Wind: " + forecastDayThreeDefaultWind + " MPH";
+                    forecastDayThreeHumidity.innerHTML = "Humidity: " + forecastDayThreeDefaultHumidity + "%";
+
+
+                    // Get day four icon
+                    var forecastDateFourIcon = 'https://openweathermap.org/img/w/' + data.list[28].weather[0].icon + '.png';
+                    //Display day four forecast info
                     forecastDayFourCityDate.innerHTML = forecastDayFourDate;
+                    forecastDayFourIcon.setAttribute('src', forecastDateFourIcon);
+                    forecastDayFourTemp.innerHTML = "Temp: " + Number(1.8 * (forecastDayFourDefaultTemp - 273) + 32).toFixed(1) + " °F";
+                    forecastDayFourWind.innerHTML = "Wind: " + forecastDayFourDefaultWind + " MPH";
+                    forecastDayFourHumidity.innerHTML = "Humidity: " + forecastDayFourDefaultHumidity + "%";
+
+
+                    // Get day five icon
+                    var forecastDateFiveIcon = 'https://openweathermap.org/img/w/' + data.list[38].weather[0].icon + '.png';
+                    //Display day five forecast info
                     forecastDayFiveCityDate.innerHTML = forecastDayFiveDate;
+                    forecastDayFiveIcon.setAttribute('src', forecastDateFiveIcon);
+                    forecastDayFiveTemp.innerHTML = "Temp: " + Number(1.8 * (forecastDayFiveDefaultTemp - 273) + 32).toFixed(1) + " °F";
+                    forecastDayFiveWind.innerHTML = "Wind: " + forecastDayFiveDefaultWind + " MPH";
+                    forecastDayFiveHumidity.innerHTML = "Humidity: " + forecastDayFiveDefaultHumidity + "%";
+
                 })
 
 
